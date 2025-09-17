507b803b90b36d5926262b3f4c800e95476e0378
UliaKaneva <juliekaneva@yandex.ru>
2025-09-17 11:39:23 +0300

    Add completed task 1.2 and Report on 1.2

--- Laba1/Laba1-2.py
+++ Laba1/Laba1-2.py
@@ -2,9 +2,43 @@ from par_and_func import *
 
 
 def main():
+    stability = 1
     matrix = read_matrix()
+    n = len(matrix)
+    if len(matrix) != len(matrix[0]):
+        raise ValueError("Матрица должна быть квадратной")
     d = read_vector()
+    pq = []
 
+    # Прямой ход метода
+    if abs(matrix[0][0]) < 10e-12:
+        raise ZeroDivisionError("Элемент b1 == 0")
+    stability *= abs(matrix[0][0]) >= abs(matrix[0][1])
+
+    pq.append((-matrix[0][1] / matrix[0][0], d[0] / matrix[0][0]))
+    for i in range(1, n - 1):
+        chasn = matrix[i][i] + matrix[i][i - 1] * pq[i - 1][0]
+        if abs(chasn) < 10e-12:
+            raise ZeroDivisionError("Деление на 0")
+        stability *= abs(matrix[i][i]) >= (abs(matrix[i][i + 1]) + abs(matrix[i][i - 1]))
+        pq.append((-matrix[i][i + 1] / chasn, (d[i] - matrix[i][i - 1] * pq[i - 1][1]) / chasn))
+    chasn = matrix[n - 1][n - 1] + matrix[n - 1][n - 2] * pq[n - 2][0]
+    if abs(chasn) < 10e-12:
+        raise ZeroDivisionError("Деление на 0")
+    pq.append((0, (d[n - 1] - matrix[n - 1][n - 2] * pq[n - 2][1]) / chasn))
+    # Обратный ход метода
+    result = [0] * n
+
+    result[-1] = pq[-1][1]
+    for i in range(n - 2, -1, -1):
+        result[i] = pq[i][0] * result[i + 1] + pq[i][1]
+    if not stability:
+        print("Предупреждение. Результат может быть посчитан с большой погрешностью!")
+
+    print("Ответ: ", end=" ")
+    for i in range(1, n + 1):
+        print(f"x{i} = {result[i - 1]}", end=" ")
+    print()
 
 
 if __name__ == "__main__":
