db624b4f69d5d4cb27257881598b0c6e36b526db
UliaKaneva <juliekaneva@yandex.ru>
2025-09-17 11:37:02 +0300

    Added instructions for entering information

--- par_and_func/Matrix.py
+++ par_and_func/Matrix.py
@@ -1,5 +1,6 @@
 def read_matrix():
-    n, m = map(int, input().split())
+    n, m = map(int, input("Введите 'n' и 'm': ").split())
+    print("Введите матрицу")
     matrix = [[float(j) for j in input().split()] for _ in range(n)]
     if any(filter(lambda x: len(x) != m, matrix)):
         raise ValueError("Количество элементов в строке матрицы не соответствует ожидаемому")
@@ -7,6 +8,7 @@ def read_matrix():
 
 
 def read_vector():
-    n = int(input())
+    n = int(input("Введите длину вектора n: "))
+    print("Введите значения вектора, каждый элемент вектора на новой строке")
     vector = [float(input()) for _ in range(n)]
     return vector
