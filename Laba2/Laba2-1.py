24264fd04989fba3702bae8b7177eafd60667084
UliaKaneva <juliekaneva@yandex.ru>
2025-10-21 11:23:06 +0300

    Add Laba2-1.py, graphic_1.py and Teoria2-1.md

---
+++ Laba2/Laba2-1.py
@@ -0,0 +1,70 @@
+import math
+
+Q = 0.72
+A, B = 0.7, 0.80
+
+
+def equation(x):
+    return ((math.sin(x) + 0.5) / 2) ** 0.5
+
+
+def f(x):
+    return math.sin(x) - 2 * x ** 2 + 0.5
+
+
+def f_fp(x):
+    return math.cos(x) - 4 * x
+
+
+def f_sp(x):
+    return -math.sin(x) - 4
+
+
+def method_simple_iteration(func, e, q, a, b):
+    cof = q / (1 - q)
+    x_last = func((a + b) / 2)
+    count_iter = 0
+
+    while abs(((x_current := func(x_last)) - x_last) * cof) >= e:
+        count_iter += 1
+        print(f"Iteration №{count_iter}: x - {x_current}; e - {x_current - x_last} ")
+        x_last = x_current
+    count_iter += 1
+    print(f"Iteration №{count_iter}: x - {x_current}; e - {x_current - x_last} ")
+    return x_current
+
+
+def search_x(func, func_sp, a, b):
+    x = a
+    step = (b - a) / 1000
+    for i in range(1000):
+        if func_sp(x) * func(x) > 0:
+            return x
+        x += step
+    raise Exception("No solution")
+
+
+def method_newton(func, func_fp, func_sp, e, a, b):
+    x_last = search_x(func, func_sp, a, b)
+    count_iter = 0
+    while abs((x_current := x_last - func(x_last) / func_fp(x_last)) - x_last) > e:
+        count_iter += 1
+        print(f"Iteration №{count_iter}: x - {x_current}; e - {x_current - x_last} ")
+        x_last = x_current
+    count_iter += 1
+    print(f"Iteration №{count_iter}: x - {x_current}; e - {x_current - x_last} ")
+    return x_current
+
+
+def main():
+    epsilon = float(input('Enter a number for epsilon: '))
+    if abs(epsilon) < 1e-14 or epsilon < 0:
+        raise ValueError('Epsilon cannot be <= 0')
+    result = method_simple_iteration(equation, epsilon, Q, A, B)
+    print(result)
+    result = method_newton(f, f_fp, f_sp, epsilon, A, B)
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Laba2/graphic_1.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-2, 2, 400)
+
+y = np.sin(x) - 2 * x ** 2 + 0.5
+x_1 = np.linspace(0.7, 0.85, 100)
+phi = ((np.sin(x_1) + 0.5) / 2) ** 0.5
+phi_pr = np.cos(x_1) / (2 * (2 * np.sin(x_1) - 1) ** 0.5)
+
+plt.figure(figsize=(10, 6))
+
+plt.plot(x, y, label='$y = sin(x) - 2x^2 + 0.5$', color='red', linewidth=2)
+plt.plot(x_1, phi, label='y = ((sin(x) + 0.5) / 2) ** 0.5', color='blue', linewidth=2)
+plt.plot(x_1, phi_pr, label='y = cos(x_1) / (2 * (2 * sin(x_1) - 1) ** 0.5)', color='black', linewidth=2)
+
+plt.title('График функции $y = sin(x) - 2 * x^2 + 0.5$')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.grid(True, alpha=0.3)
+plt.axhline(y=0, color='k', linewidth=0.5)
+plt.axvline(x=0, color='k', linewidth=0.5)
+
+plt.show()
