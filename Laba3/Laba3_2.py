ba94caa16352eb506b5ff309929eb83fd9b4df39
UliaKaneva <juliekaneva@yandex.ru>
2025-11-30 14:00:00 +0300

    qwerty

--- Laba3/Laba3_2.py
+++ Laba3/Laba3_2.py
@@ -83,34 +83,50 @@ def main():
     f_list = [-1.2490, -0.78540, 0.78540, 1.2490, 1.3734]
     # x_list = [0.0, 1.0, 2.0, 3.0, 4.0]
     # f_list = [0.0, 1.8415, 2.9093, 3.1411, 3.2432]
+    f_list = [0.06334, 0.06334, 0.06334, 0.06334, 0.06334, 0.06334,
+              0.06334, 0.04433, 0.03719, 0.03377,
+              0.03217, 0.03172, 0.0326, 0.03501,
+              0.03885, 0.04417, 0.05112, 0.05993,
+              0.07087, 0.08428, 0.10056, 0.12013]
+
+    x_list = [0.0, 0.04, 0.08, 0.16, 0.175, 0.185,
+              0.19235, 0.22236, 0.23789, 0.25006,
+              0.26043, 0.2697, 0.27466, 0.28836,
+              0.31015, 0.34034, 0.37979, 0.42976,
+              0.49183, 0.56794, 0.66027, 0.77136]
     x_star = -0.5
     # x_star = 1.5
+    x_star = 0.3
 
     res_n, res_str = splain(x_list, f_list, x_star, is_print=True)
     print(res_n)
     print(res_str)
 
     # График
-    x1 = np.linspace(x_list[0] - 1.0, x_list[1] + 1.0, 100)
-    x2 = np.linspace(x_list[1] - 1.0, x_list[2] + 1.0, 100)
-    x3 = np.linspace(x_list[2] - 1.0, x_list[3] + 1.0, 100)
-    x4 = np.linspace(x_list[3] - 1.0, x_list[4] + 1.0, 100)
+    x_s = []
+    p_s = []
+    m_p_s = []
+    len_us = 0.0
     c_m = generating_coefficient_equations(x_list, f_list)
-    p1 = spline_for_graphic(x_list, x1, c_m, 0)
-    p2 = spline_for_graphic(x_list, x2, c_m, 1)
-    p3 = spline_for_graphic(x_list, x3, c_m, 2)
-    p4 = spline_for_graphic(x_list, x4, c_m, 3)
+    for i in range(len(x_list) - 1):
+        x_s.append(np.linspace(x_list[i] - len_us, x_list[i + 1] + len_us, 100))
+        p_s.append(spline_for_graphic(x_list, x_s[i], c_m, i))
+
 
     fig, ax1 = plt.subplots(figsize=(10, 6))
-
-    ax1.plot(x1, p1, label='Кубический сплайн', color='#1f77b4', linewidth=2)
-    ax1.plot(x2, p2, color='#ff7f0e', linewidth=2)
-    ax1.plot(x3, p3, color='#2ca02c', linewidth=2)
-    ax1.plot(x4, p4, color='#d62728', linewidth=2)
-    ax1.scatter(x_list, f_list, color='#FF1493', s=50, zorder=5, label='Узлы интерполяции', edgecolors='black',
-                linewidth=1)  # Ярко-розовый
-    ax1.scatter(x_star, spline_for_graphic(x_list, x_star, c_m, 1), color='#FFD700', s=80, zorder=5, label='$X^*$',
-                edgecolors='black', linewidth=1)  # Золотой
+    ax1.plot(x_s[0], p_s[0], label='Кубический сплайн', color='#2ca02c', linewidth=2)
+    ax1.plot(x_s[0], -p_s[0], label='Кубический сплайн', color='#2ca02c', linewidth=2)
+    for i in range(1, len(x_s)):
+        ax1.plot(x_s[i], p_s[i], color='#2ca02c', linewidth=2)
+        ax1.plot(x_s[i], -p_s[i], color='#2ca02c', linewidth=2)
+    # ax1.plot(x1, p1, label='Кубический сплайн', color='#1f77b4', linewidth=2)
+    # ax1.plot(x2, p2, color='#ff7f0e', linewidth=2)
+    # ax1.plot(x3, p3, color='#2ca02c', linewidth=2)
+    # ax1.plot(x4, p4, color='#d62728', linewidth=2)
+    # ax1.scatter(x_list, f_list, color='#FF1493', s=50, zorder=5, label='Узлы интерполяции', edgecolors='black',
+    #             linewidth=1)  # Ярко-розовый
+    # ax1.scatter(x_star, spline_for_graphic(x_list, x_star, c_m, 7), color='#FFD700', s=80, zorder=5, label='$X^*$',
+    #             edgecolors='black', linewidth=1)  # Золотой
     ax1.set_title(f'Сплайн')
     ax1.set_xlabel('x')
     ax1.set_ylabel('y')
@@ -118,6 +134,7 @@ def main():
     ax1.grid(True, alpha=0.3)
     ax1.axhline(y=0, color='k', linewidth=0.5)
     ax1.axvline(x=0, color='k', linewidth=0.5)
+    plt.axis('equal')
 
     plt.tight_layout()
     plt.show()
--- Laba3/Laba3_4.py
+++ Laba3/Laba3_4.py
@@ -6,7 +6,7 @@ def first_derivative(x_list, f_list, x_star):
             break
     if index == -1:
         raise ValueError(f"{x_star} is not in [{x_list[0]}, {x_list[-1]}]")
-    if index == len(x_list) - 2:
+    if index == len(x_list) - 2 or index == 0:
         return (f_list[index + 1] - f_list[index]) / (x_list[index + 1] - x_list[index])
     b = (f_list[index + 1] - f_list[index]) / (x_list[index + 1] - x_list[index])
     k1 = (f_list[index + 2] - f_list[index + 1]) / (x_list[index + 2] - x_list[index + 1])
@@ -18,7 +18,7 @@ def first_derivative(x_list, f_list, x_star):
 def second_derivative(x_list, f_list, x_star):
     index = -1
     for i in range(len(x_list) - 1):
-        if x_list[i] <= x_star <= x_list[i + 1]:
+        if x_list[i] < x_star <= x_list[i + 1]:
             index = i
             break
 
@@ -36,10 +36,14 @@ def main():
     # x_list = [0.0, 0.1, 0.2, 0.3, 0.4]
     # f_list = [1.0, 1.1052, 1.2214, 1.3499, 1.4918]
     # x_star = 0.2
+    # x_list = [-1.0, 0.0, 1.0, 2.0, 3.0]
+    # f_list = [-1.7854, 0.0, 1.7854, 3.1071, 4.249]
+    # x_star = 1.0
+
     f_d = first_derivative(x_list, f_list, x_star)
-    print(f"Первая производная: {f_d:.5f}")
+    print(f"Первая производная в точке {x_star}: {f_d:.5f}")
     s_d = second_derivative(x_list, f_list, x_star)
-    print(f"Вторая производная: {s_d:.5f}")
+    print(f"Вторая производная в точке {x_star}: {s_d:.5f}")
 
 
 if __name__ == "__main__":
